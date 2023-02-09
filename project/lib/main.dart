6cb098ca15eb99c808d46c390570f1ada367321a
KAAzadi <70913312+KAAzadi@users.noreply.github.com>
2023-02-09 17:03:15 -0500

    Pages Created
    Created starter pages for account creation and account information.

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'account_info.dart';
+import 'create_account.dart';
 
 void main() {
   runApp(const MyApp());
@@ -53,28 +55,66 @@ class _MyHomePageState extends State<MyHomePage> {
               image: NetworkImage("https://cdn.pixabay.com/photo/2016/09/09/23/27/the-ostrich-1658267_960_720.jpg"),
             ),
             Container(
-                child: Column(
-                  children: <Widget>[
-                    TextField(
-                        decoration: InputDecoration(
-                          border: OutlineInputBorder(),
-                          labelText: "Username",
-                        )
-                    ),
-                    TextField(
+              child: Column(
+                children: <Widget>[
+                  TextField(
                       decoration: InputDecoration(
                         border: OutlineInputBorder(),
-                        labelText: "Password",
-                      ),
-                      obscureText: true,
+                        labelText: "Username",
+                      )
+                  ),
+                  TextField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: "Password",
                     ),
-                    TextButton(
-                      onPressed: (){},
-                      child: Text("Click here to register"),
-                    )
-                  ],
-                )
-            )
+                    obscureText: true,
+                  ),
+                  TextButton(
+                    onPressed: (){},
+                    child: Text("Click here to register"),
+                  ),
+                  ElevatedButton(onPressed: (){}, child: Text("LOGIN")),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children:[
+                Container(
+                  child: Column(
+                    children: [
+                      Text("Account creation"),
+                      IconButton(
+                        onPressed: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const CreateAccount(title: "Create Account")),
+                          );
+                        },
+                        icon: Icon(Icons.create),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  child: Column(
+                    children: [
+                      Text("My Account"),
+                      IconButton(
+                        onPressed: (){
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => const AccountInfo(title: "My Account")),
+                          );
+                        },
+                        icon: Icon(Icons.create),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ],
         ),
       ),
