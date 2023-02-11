36c4c20c448ff38d9c9b24b0fa95f6e615b6a258
Varsha V <63315944+vvattap@users.noreply.github.com>
2023-02-11 09:05:55 -0500

    sign up/log in/log out email+password functionality

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -1,8 +1,14 @@
+// ignore_for_file: avoid_unnecessary_containers
+
 import 'package:flutter/material.dart';
 import 'account_info.dart';
 import 'create_account.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -40,6 +46,47 @@ class _MyHomePageState extends State<MyHomePage> {
       _counter++;
     });
   }
+  //have to clean up the controllers after DISPOSE
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  Future<void> createAccount() async {
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  Future<void> login() async {
+    try {
+      final credential = await FirebaseAuth.instance.signInWithEmailAndPassword(
+        //will want to have separate controllers?
+        email: _emailController.text.trim(),
+        password: _passwordController.text.trim(),
+      );
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided for that user.');
+      }
+    }
+  }
+  Future<void> logout() async {
+    print("logging out");
+    await FirebaseAuth.instance.signOut();
+
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,45 +98,59 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Image(
-              image: NetworkImage("https://cdn.pixabay.com/photo/2016/09/09/23/27/the-ostrich-1658267_960_720.jpg"),
+            const Image(
+              image: NetworkImage(
+                  "https://cdn.pixabay.com/photo/2016/09/09/23/27/the-ostrich-1658267_960_720.jpg"),
             ),
             Container(
               child: Column(
                 children: <Widget>[
-                  TextField(
-                      decoration: InputDecoration(
+                   TextField(
+                      controller: _emailController ,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: "Username",
-                      )
-                  ),
+                      )),
                   TextField(
-                    decoration: InputDecoration(
+                    //try visiblePassword and then try text?
+                    controller: _passwordController,
+                    keyboardType: TextInputType.visiblePassword,
+                    decoration: const InputDecoration(
                       border: OutlineInputBorder(),
                       labelText: "Password",
                     ),
                     obscureText: true,
                   ),
                   TextButton(
-                    onPressed: (){},
-                    child: Text("Click here to register"),
+                    onPressed: () {
+                      createAccount();
+                    },
+                    child: const Text("Click here to register"),
                   ),
-                  ElevatedButton(onPressed: (){}, child: Text("LOGIN")),
+                  ElevatedButton(onPressed: () {
+                    login();
+                  }, child: const Text("LOGIN")),
+                  ElevatedButton(onPressed: (){
+                    logout();
+                  }, child: const Text("Logout")),
                 ],
               ),
             ),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children:[
+              children: [
                 Container(
                   child: Column(
                     children: [
                       Text("Account creation"),
                       IconButton(
-                        onPressed: (){
+                        onPressed: () {
                           Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => const CreateAccount(title: "Create Account")),
+                            MaterialPageRoute(
+                                builder: (context) => const CreateAccount(
+                                    title: "Create Account")),
                           );
                         },
                         icon: Icon(Icons.create),
@@ -102,10 +163,12 @@ class _MyHomePageState extends State<MyHomePage> {
                     children: [
                       Text("My Account"),
                       IconButton(
-                        onPressed: (){
+                        onPressed: () {
                           Navigator.push(
                             context,
-                            MaterialPageRoute(builder: (context) => const AccountInfo(title: "My Account")),
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    const AccountInfo(title: "My Account")),
                           );
                         },
                         icon: Icon(Icons.create),
