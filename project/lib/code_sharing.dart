392700cb9530e494ec3b9cd6f75c563a4bfb708d
Varsha V <63315944+vvattap@users.noreply.github.com>
2023-02-23 14:33:05 -0500

    code sharing page now pulling teamId to share as a code to join group

--- project/lib/code_sharing.dart
+++ project/lib/code_sharing.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class CodeSharing extends StatefulWidget {
   const CodeSharing({super.key, required this.title});
@@ -19,6 +21,25 @@ class _CodeSharingState extends State<CodeSharing> {
   }
 
   @override
+  void initState() {
+    super.initState();
+    grabTeamId();
+  }
+
+  String? teamId;
+  Future<void> grabTeamId() async {
+    String? uid = FirebaseAuth.instance.currentUser?.uid;
+    DatabaseReference ref = FirebaseDatabase.instance.ref("users/$uid");
+    final snapshot = await ref.child('teamId').get();
+    if (snapshot.exists) {
+      teamId = (snapshot.value).toString();
+      print(snapshot.value);
+    } else {
+      print('No data available.');
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -29,14 +50,15 @@ class _CodeSharingState extends State<CodeSharing> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             const Icon(size: 250, Icons.qr_code_2),
-            const Text("Random Code Placeholder"),
+            Text("$teamId"),
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 const Text("Link Here"),
                 IconButton(
                     onPressed: () {
-                      print("generate code");
+                      print("calling grabTeamId");
+                      grabTeamId();
                     },
                     icon: const Icon(size: 30, Icons.content_copy_rounded))
               ],
