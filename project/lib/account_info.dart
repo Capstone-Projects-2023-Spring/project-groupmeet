533d5d272d85698e71294de6fd21c9109ac39a22
KAAzadi <70913312+KAAzadi@users.noreply.github.com>
2023-02-28 15:11:55 -0500

    Update account_info.dart

--- project/lib/account_info.dart
+++ project/lib/account_info.dart
@@ -1,26 +1,39 @@
 import 'package:flutter/material.dart';
 import 'socials.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class AccountInfo extends StatefulWidget {
-  const AccountInfo({super.key, required this.title});
+  const AccountInfo({super.key, required this.title, required this.ref});
 
   final String title;
+  final DatabaseReference ref;
 
   @override
   State<AccountInfo> createState() => _AccountInfoState();
 }
 
 class _AccountInfoState extends State<AccountInfo> {
-  // int _counter = 0;
+  String name = "";
+  String email = "";
+  void GetData()async{
+    DatabaseEvent event = await widget.ref.once();
+    if(event.snapshot.exists) {
+      name = event.snapshot
+          .child("firstName")
+          .value.toString() + " " + event.snapshot
+          .child("lastName")
+          .value.toString();
 
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
+      email = event.snapshot.child("email").value.toString();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
+    GetData();
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -35,10 +48,26 @@ class _AccountInfoState extends State<AccountInfo> {
                   context,
                   MaterialPageRoute(
                       builder: (context) =>
-                          const SocialMedia(title: "Social Media")),
+                      const SocialMedia(title: "Social Media")),
                 );
               },
               icon: const Icon(Icons.create),
+            ),
+            Container(
+              child: Column(
+                  children: [
+                    Text("Name:"),
+                    Text(name),
+                  ]
+              ),
+            ),
+            Container(
+              child: Column(
+                  children: [
+                    Text("Email:"),
+                    Text(email),
+                  ]
+              ),
             ),
           ],
         ),
