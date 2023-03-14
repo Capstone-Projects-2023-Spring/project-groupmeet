ae622ff4d419682f8d42576c5f05d981557fbe66
tweiss9 <tylerhweiss@gmail.com>
2023-03-14 13:18:52 -0400

    fixed error cause when i hit edit social media more styling

--- project/lib/account_info.dart
+++ project/lib/account_info.dart
@@ -5,7 +5,8 @@ import 'package:firebase_database/firebase_database.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class AccountInfo extends StatefulWidget {
-  const AccountInfo({Key? key, required this.title, required this.ref}) : super(key: key);
+  const AccountInfo({Key? key, required this.title, required this.ref})
+      : super(key: key);
 
   final String title;
   final DatabaseReference ref;
@@ -34,7 +35,8 @@ class _AccountInfoState extends State<AccountInfo> {
     DatabaseEvent event = await widget.ref.once();
     if (event.snapshot.exists) {
       setState(() {
-        name = "${event.snapshot.child("firstName").value} ${event.snapshot.child("lastName").value}";
+        name =
+            "${event.snapshot.child("firstName").value} ${event.snapshot.child("lastName").value}";
         email = event.snapshot.child("email").value.toString();
       });
     }
@@ -49,18 +51,6 @@ class _AccountInfoState extends State<AccountInfo> {
       body: Center(
         child: Column(
           children: [
-            const Text("Edit Social Media Accounts"),
-            IconButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => SocialMedia(title: "Social Media", databaseReference: ref),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.create),
-            ),
             Column(
               children: [
                 const Text("Name:"),
@@ -75,14 +65,28 @@ class _AccountInfoState extends State<AccountInfo> {
             ),
             const Text("Edit Account Information"),
             IconButton(
-              onPressed: () async{
+              onPressed: () async {
                 await Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (context) => EditAccountInfo(title: "Edit My Account", ref: ref),
+                    builder: (context) =>
+                        EditAccountInfo(title: "Edit My Account", ref: ref),
                   ),
                 );
                 getData();
+              },
+              icon: const Icon(Icons.create),
+            ),
+            const Text("Edit Social Media Accounts"),
+            IconButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => SocialMedia(
+                        title: "Social Media", databaseReference: ref),
+                  ),
+                );
               },
               icon: const Icon(Icons.create),
             ),
