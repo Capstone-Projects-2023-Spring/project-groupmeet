39429b09f1383b6f12321ead5864543619644aa3
jaredStef <jaredStef@iCloud.com>
2023-04-06 04:23:02 -0400

    Add New Group Creation UI and New Group Loading

--- project/lib/home.dart
+++ project/lib/home.dart
@@ -1,14 +1,34 @@
+import 'dart:collection';
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:groupmeet/new_group_creation.dart';
 import 'package:groupmeet/theme.dart';
 import 'code_reception.dart';
 import 'group_creation.dart';
 import 'settings.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key, required String title});
+class Group {
+  Color color;
+  String emoji;
+  String name;
 
-  get title => null;
+  Group(this.color, this.emoji, this.name);
+}
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreen createState() => _HomeScreen();
+}
+
+class _HomeScreen extends State<HomeScreen> {
+
+  List<Group> displayedGroups = [];
+
+  bool observing = false;
 
   // TODO: Cross-Platform working QR Screen
   void showQR(context) {
@@ -24,7 +44,7 @@ class HomeScreen extends StatelessWidget {
     Navigator.of(context).push(
       platformPageRoute(
           context: context,
-          builder: (context) => const GroupCreation(title: "Group Creation")),
+          builder: (context) => NewGroupCreation()),
     );
   }
 
@@ -41,8 +61,62 @@ class HomeScreen extends StatelessWidget {
     print("Tapped group $group");
   }
 
+  void observeGroups() {
+    if (observing) {
+      return;
+    }
+
+    String? userID = FirebaseAuth.instance.currentUser?.uid;
+
+    if (userID == null) {
+      // TODO: Alert saying not logged in yet?
+      print("Not logged in");
+    }
+
+    FirebaseDatabase.instance.ref("users/${userID!}/groupIds").onValue.listen((event) async {
+      if (event.snapshot.value == null) {
+        return;
+      }
+
+      print(event.snapshot.value);
+
+      Iterable<Object?> groups = (event.snapshot.value as Map<Object?, Object?>).keys;
+
+      List<Group> newGroups = [];
+
+      for (Object? groupID in groups) {
+        String groupIDCasted = groupID as String;
+        final groupInfo = await FirebaseDatabase.instance.ref("groups/$groupIDCasted/").get();
+
+        if (!groupInfo.exists) {
+          continue;
+        }
+        print(groupInfo.value);
+
+        Map<Object?, Object?> vals = groupInfo.value as Map<Object?, Object?>;
+
+        int color = vals['color'] as int;
+        String emoji = vals['emoji'] as String;
+        String name = vals['name'] as String;
+
+        print(color);
+        print(emoji);
+        print(name);
+
+        newGroups.add(Group(Color(color), emoji, name));
+      }
+
+      setState(() => displayedGroups = newGroups);
+    });
+
+    observing = true;
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    observeGroups();
+
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
 
@@ -60,7 +134,7 @@ class HomeScreen extends StatelessWidget {
                     top: 48 + MediaQuery.of(context).viewPadding.top + 8 + 8),
                 crossAxisCount: 2,
                 mainAxisSpacing: 0,
-                children: List.generate(3, (index) {
+                children: List.generate(displayedGroups.length, (index) {
                   return GestureDetector(
                     onTap: () => selectedGroup(index),
                     child: Column(
@@ -70,12 +144,12 @@ class HomeScreen extends StatelessWidget {
                           alignment: Alignment.center,
                           children: [
                             ColorFiltered(
-                              colorFilter: const ColorFilter.mode(
-                                  roundPurple, BlendMode.srcIn),
+                              colorFilter: ColorFilter.mode(
+                                  displayedGroups[index].color, BlendMode.srcIn),
                               child: Image.asset("images/GroupRound.png",
                                   width: 120, height: 120, isAntiAlias: true),
                             ),
-                            PlatformText("🖥️️",
+                            PlatformText(displayedGroups[index].emoji,
                                 style: const TextStyle(
                                   fontSize: 40,
                                   fontWeight: FontWeight.bold,
@@ -85,7 +159,7 @@ class HomeScreen extends StatelessWidget {
                         Padding(
                           padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
                           child: PlatformText(
-                            "STEM Baddies",
+                            displayedGroups[index].name,
                             style: const TextStyle(
                                 fontWeight: FontWeight.w600, fontSize: 14),
                             textAlign: TextAlign.center,
---
+++ project/lib/new_group_creation.dart
@@ -0,0 +1,287 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_material_color_picker/flutter_material_color_picker.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+import 'package:groupmeet/new_calendar_selection.dart';
+import 'package:groupmeet/theme.dart';
+
+import 'signin.dart';
+
+class NewGroupCreation extends StatefulWidget {
+  @override
+  _NewGroupCreationState createState() => _NewGroupCreationState();
+}
+
+class _NewGroupCreationState extends State<NewGroupCreation> {
+
+  _NewGroupCreationState() {
+    selectedColor = createMaterialColor(roundPurple);
+  }
+
+  MaterialColor createMaterialColor(Color color) {
+    List strengths = <double>[.05];
+    Map<int, Color> swatch = {};
+    final int r = color.red, g = color.green, b = color.blue;
+
+    for (int i = 1; i < 10; i++) {
+      strengths.add(0.1 * i);
+    }
+    for (var strength in strengths) {
+      final double ds = 0.5 - strength;
+      swatch[(strength * 1000).round()] = Color.fromRGBO(
+        r + ((ds < 0 ? r : (255 - r)) * ds).round(),
+        g + ((ds < 0 ? g : (255 - g)) * ds).round(),
+        b + ((ds < 0 ? b : (255 - b)) * ds).round(),
+        1,
+      );
+    }
+    return MaterialColor(color.value, swatch);
+  }
+
+  MaterialColor? selectedColor;
+  MaterialColor? tempColor;
+
+  String name = "";
+  String emoji = "";
+
+  late DatabaseReference ref;
+  String? uid;
+
+  void changedName(String string) {
+    name = string;
+  }
+
+  void changedEmoji(String string) {
+    emoji = string;
+  }
+
+  Future<void> buttonPress(BuildContext context) async {
+    if(name.trim().isEmpty || emoji.trim().characters.length != 1) {
+      PlatformAlertDialog error = PlatformAlertDialog(
+        title: PlatformText("Whoops!"),
+        content: PlatformText(
+            'Please enter a group name and 1 character emoji. You may optionally select a non-default color'),
+        actions: [
+          PlatformTextButton(
+            child: PlatformText("Ok",
+                selectionColor: roundPurple,
+                style: const TextStyle(color: roundPurple)),
+            onPressed: () => Navigator.of(context).pop(),
+          )
+        ],
+      );
+
+      showPlatformDialog(
+        context: context,
+        builder: (context) {
+          return error;
+        },
+      );
+      return;
+    }
+
+    // TODO: Create Group in Firebase and Dismiss
+    // TODO: Load User Groups to home screen :)
+    // TODO: Then Accept Screen, Viewing, and Settings
+
+    String? userID = FirebaseAuth.instance.currentUser?.uid;
+
+    if (userID == null) {
+
+      PlatformAlertDialog error = PlatformAlertDialog(
+        title: PlatformText("Whoops!"),
+        content: PlatformText(
+            'You are not logged in. Log back in or check your internet connection'),
+        actions: [
+          PlatformTextButton(
+            child: PlatformText("Ok",
+                selectionColor: roundPurple,
+                style: const TextStyle(color: roundPurple)),
+            onPressed: () => Navigator.of(context).pop(),
+          )
+        ],
+      );
+
+      showPlatformDialog(
+        context: context,
+        builder: (context) {
+          return error;
+        },
+      );
+      return;
+    }
+
+    final DatabaseReference groupRef =
+    FirebaseDatabase.instance.ref().child('groups').push();
+
+    await groupRef.set({
+      'name': name.trim(),
+      'emoji': emoji.trim(),
+      'admin': userID,
+      'members': {userID: true},
+      'color': selectedColor!.shade500.value
+    });
+
+    await FirebaseDatabase.instance
+        .ref("users/$userID/groupIds/${groupRef.key}").set(true);
+
+    Navigator.of(context).pop();
+  }
+
+  void signIn(BuildContext context) {
+    Navigator.of(context).push(
+        platformPageRoute(context: context, builder: (context) => NewSignIn()));
+  }
+
+  void _openDialog(String title, Widget content) {
+    showDialog(
+      context: context,
+      builder: (_) {
+        return AlertDialog(
+          title: PlatformText(title),
+          content: content,
+          actions: [
+            TextButton(
+              child: PlatformText('Cancel', selectionColor: roundPurple),
+              onPressed: Navigator.of(context).pop,
+            ),
+            TextButton(
+              child: PlatformText('Submit', selectionColor: roundPurple),
+              onPressed: () {
+                Navigator.of(context).pop();
+                setState(() => selectedColor = tempColor);
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void colorTapped() {
+    _openDialog(
+      "Color Picker",
+        MaterialColorPicker(
+          selectedColor: selectedColor,
+          colors: [
+            createMaterialColor(roundPurple),
+            createMaterialColor(roundWhite),
+            createMaterialColor(roundRed),
+            createMaterialColor(roundOrange),
+            createMaterialColor(roundYellow),
+            createMaterialColor(roundBlue),
+            createMaterialColor(roundPink),
+            createMaterialColor(roundLightPurple),
+            createMaterialColor(roundGreen),
+            createMaterialColor(roundSilver),
+            createMaterialColor(roundForest),
+            createMaterialColor(roundTeal)
+          ],
+          allowShades: false,
+          onMainColorChange: (color) => setState(() => tempColor = color as MaterialColor?),
+
+        )
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    CircleColor colorCircle = CircleColor(color: selectedColor!.shade500, circleSize: 32,);
+
+    return PlatformScaffold(
+        appBar: PlatformAppBar(title: PlatformText("New Circle")),
+        body: Center(
+            child: Column(
+              children: [
+                SizedBox(
+                    width: screenWidth,
+                    height:
+                    MediaQuery.of(context).viewPadding.top + 0.08 * screenHeight),
+                Image.asset(
+                  "images/AddPhoto.png",
+                  height: 160,
+                  width: screenWidth,
+                  isAntiAlias: true,
+                ),
+                SizedBox(width: screenWidth, height: 8),
+                PlatformText("Get Around",
+                    style: const TextStyle(fontSize: 36, fontWeight: FontWeight.w600),
+                    textAlign: TextAlign.center),
+                SizedBox(width: screenWidth, height: 32),
+
+                SizedBox(
+                    width: screenWidth * (3 / 4),
+                    child: PlatformTextField(
+                      hintText: "Group Name",
+                      autofocus: true,
+                      cursorColor: roundPurple,
+                      onChanged: (p0) => changedName(p0),
+                      material: (_, __) => MaterialTextFieldData(
+                          decoration: const InputDecoration(
+                              focusColor: roundPurple, hoverColor: roundPurple)),
+                    )),
+                SizedBox(
+                  width: screenWidth,
+                  height: 16,
+                ),
+                Row(
+                  children: [
+                    SizedBox(width: screenWidth / 8),
+                    SizedBox(
+                        width: screenWidth * (3 / 8),
+                        child: PlatformTextField(
+                          hintText: "Emoji",
+                          keyboardType: TextInputType.text,
+                          cursorColor: roundPurple,
+                          onChanged: (p0) => changedEmoji(p0),
+                        )),
+                        SizedBox(width: screenWidth / 16,),
+                        PlatformText("Color", style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600)),
+                        SizedBox(width: screenWidth / 16,),
+                        GestureDetector(
+                          child: colorCircle,
+                          onTap: () => colorTapped(),
+                        )
+                  ],
+                ),
+
+                Expanded(
+                  child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                            height: 64,
+                            width: 64,
+                            child: PlatformIconButton(
+                              icon: Image.asset(
+                                "images/OnboardingNext.png",
+                                height: 64,
+                                width: 64,
+                                isAntiAlias: true,
+                              ),
+                              padding: EdgeInsets.zero,
+                              onPressed: () => buttonPress(context),
+                            )),
+                        SizedBox(width: screenWidth, height: 16),
+                        PlatformText("© 2023 Round Corp\nFrom Philly with Love 🤍",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 10)),
+                        SizedBox(
+                          width: screenWidth,
+                          height: 32,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            )));
+  }
+}
