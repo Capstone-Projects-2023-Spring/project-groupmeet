3487abf49d6c5a8519e06d0701bb526820e19dc0
tweiss9 <tylerhweiss@gmail.com>
2023-04-09 13:14:20 -0400

    prompt notifications

--- project/lib/main.dart
+++ project/lib/main.dart
@@ -6,22 +6,20 @@ import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:groupmeet/explainer.dart';
 import 'package:groupmeet/theme.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:permission_handler/permission_handler.dart';
 
 import 'home.dart';
-import 'new_group_creation.dart';
 
-// Initialize the app and run it.
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Permission.notification.request();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
-// The main app widget.
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
-  // Build the app widget.
   @override
   Widget build(BuildContext context) {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.light);
@@ -76,12 +74,14 @@ class MyApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         material: (context, platform) =>
             MaterialAppData(theme: materialTheme, color: roundPurple),
-        cupertino: (context, platform) =>
-            CupertinoAppData(theme: cupertinoTheme, color: roundPurple, localizationsDelegates: [
-              DefaultCupertinoLocalizations.delegate,
-              DefaultMaterialLocalizations.delegate,
-              DefaultWidgetsLocalizations.delegate,
-            ]),
+        cupertino: (context, platform) => CupertinoAppData(
+                theme: cupertinoTheme,
+                color: roundPurple,
+                localizationsDelegates: [
+                  DefaultCupertinoLocalizations.delegate,
+                  DefaultMaterialLocalizations.delegate,
+                  DefaultWidgetsLocalizations.delegate,
+                ]),
         home: firstStop, //Explainer(pageNo: 0),
         title: "Round",
         color: roundPurple);
