11e04ef6439c0be289b93e8885884c214df7c850
Jerrell <tuk57480@temple.edu>
2023-04-10 01:53:21 -0400

    Addition of display_code.dart, addition of 'Get Group Code' option in Code sharing, a few name changes

--- project/lib/code_sharing.dart
+++ project/lib/code_sharing.dart
@@ -3,7 +3,9 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:groupmeet/DisplayQr.dart';
+import 'package:groupmeet/display_qr.dart';
+
+import 'display_code.dart';
 
 class CodeSharing extends StatefulWidget {
   const CodeSharing({Key? key, required this.title}) : super(key: key);
@@ -25,10 +27,10 @@ class _CodeSharingState extends State<CodeSharing> {
 
   void getQr() async {
     List <dynamic> groupName = [];
-    List <dynamic> name_id = [];
-    String? user_ex = FirebaseAuth.instance.currentUser!.uid;
+    List <dynamic> nameId = [];
+    String? userEx = FirebaseAuth.instance.currentUser!.uid;
     DatabaseReference ref = FirebaseDatabase.instance.ref();
-    final snapshot = await ref.child("users/$user_ex/groupIds").get();
+    final snapshot = await ref.child("users/$userEx/groupIds").get();
     if (snapshot.exists) {
       //Get groupsIds then send search for groupname
       Map<dynamic, dynamic> type = snapshot.value as Map<dynamic, dynamic>;
@@ -37,9 +39,44 @@ class _CodeSharingState extends State<CodeSharing> {
         final snapshot2 = await ref.child("groups/${keys.key.toString()}/name")
             .get();
         groupName.add(snapshot2.value);
-        name_id.add(keys.key);
+        nameId.add(keys.key);
       }
-      Navigator.push(context, MaterialPageRoute(builder: (context) => Display(groupName, name_id)));
+      Navigator.push(context,
+          MaterialPageRoute(builder: (context) => Display(groupName, nameId)));
+    }
+    else {
+      Fluttertoast.showToast(
+          msg: "You are not apart of any groups! Please create one or add yourself to one",
+          toastLength: Toast.LENGTH_LONG,
+          gravity: ToastGravity.BOTTOM,
+          timeInSecForIosWeb: 4,
+          backgroundColor: Colors.grey,
+          fontSize: 15);
+      Navigator.of(context).pop(false);
+    }
+  }
+
+  void getCode() async {
+    List <dynamic> groupName = [];
+    List <dynamic> nameId = [];
+    String? userEx = FirebaseAuth.instance.currentUser!.uid;
+    DatabaseReference ref = FirebaseDatabase.instance.ref();
+    final snapshot = await ref.child("users/$userEx/groupIds").get();
+    if (snapshot.exists) {
+      //Get groupsIds then send search for groupname
+      Map<dynamic, dynamic> type = snapshot.value as Map<dynamic, dynamic>;
+      for (var keys in type.entries) {
+        //call to ref is a single instance where it doesn't loop. Figure it out.
+        final snapshot2 = await ref.child("groups/${keys.key.toString()}/name")
+            .get();
+        //options to add emojis or colors to group for easier recognition
+        //final snapshot3 = await ref.child("groups/${keys.key.toString()}/emoji").get();
+        groupName.add(snapshot2.value);
+        nameId.add(keys.key);
+      }
+      Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => displayCode(groupName, nameId)));
     }
     else {
       Fluttertoast.showToast(
@@ -54,79 +91,91 @@ class _CodeSharingState extends State<CodeSharing> {
   }
 
 
-  @override
-  Widget build(BuildContext context) {
-    return PlatformScaffold(
-      appBar: PlatformAppBar(
-        title: PlatformText(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Icon(size: 30, PlatformIcons(context).shareSolid),
-            FutureBuilder<DataSnapshot>(
-              future: _grabGroupId(),
-              builder: (context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.waiting) {
-                  return PlatformCircularProgressIndicator();
-                }
-                if (snapshot.hasError) {
-                  return PlatformText('Error: ${snapshot.error}');
-                }
-                if (!snapshot.hasData) {
-                  return PlatformText('No data available');
-                }
-                String? groupId = snapshot.data!.value.toString();
-                return Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    PlatformText("Join Group: $groupId"),
-                    PlatformIconButton(
-                      onPressed: () {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
-                            content: PlatformText('Join Group Button'),
-                            duration: const Duration(seconds: 5),
-                          ),
-                        );
-                      },
-                      icon: Icon(size: 30, PlatformIcons(context).share),
-                    ),
-                  ],
-                );
-              },
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                PlatformText("Link Here"),
-                PlatformIconButton(
-                  onPressed: () {
-                    ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(
-                        content: Text('Link Here Button'),
-                        duration: Duration(seconds: 5),
-                      ),
-                    );
-                  },
-                  icon: Icon(size: 30, PlatformIcons(context).share),
-                ),
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                PlatformText("Get QR Code(s) Here"),
-                PlatformIconButton(
-                  onPressed: getQr,
-                  icon: Icon(size: 30, IconData(0xe4f7, fontFamily: 'MaterialIcons')),
-                ),
-              ],
-            ),
-          ],
+    @override
+    Widget build(BuildContext context) {
+      return PlatformScaffold(
+        appBar: PlatformAppBar(
+          title: PlatformText(widget.title),
         ),
-      ),
-    );
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Icon(size: 30, PlatformIcons(context).shareSolid),
+              FutureBuilder<DataSnapshot>(
+                future: _grabGroupId(),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return PlatformCircularProgressIndicator();
+                  }
+                  if (snapshot.hasError) {
+                    return PlatformText('Error: ${snapshot.error}');
+                  }
+                  if (!snapshot.hasData) {
+                    return PlatformText('No data available');
+                  }
+                  String? groupId = snapshot.data!.value.toString();
+                  return Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      PlatformText("Join Group: $groupId"),
+                      PlatformIconButton(
+                        onPressed: () {
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: PlatformText('Join Group Button'),
+                              duration: const Duration(seconds: 5),
+                            ),
+                          );
+                        },
+                        icon: Icon(size: 30, PlatformIcons(context).share),
+                      ),
+                    ],
+                  );
+                },
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  PlatformText("Link Here"),
+                  PlatformIconButton(
+                    onPressed: () {
+                      ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(
+                          content: Text('Link Here Button'),
+                          duration: Duration(seconds: 5),
+                        ),
+                      );
+                    },
+                    icon: Icon(size: 30, PlatformIcons(context).share),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  PlatformText("Get QR Code(s) Here"),
+                  PlatformIconButton(
+                    onPressed: getQr,
+                    icon: const Icon(size: 30,
+                        IconData(0xe4f7, fontFamily: 'MaterialIcons')),
+                  ),
+                ],
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  PlatformText("Get Group Code"),
+                  PlatformIconButton(
+                    onPressed: getCode,
+                    icon: const Icon(size: 30,
+                        IconData(0xe050, fontFamily: 'MaterialIcons')),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      );
+    }
   }
-}
---
+++ project/lib/display_code.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_platform_widgets/flutter_platform_widgets.dart';
+
+class displayCode extends StatelessWidget{
+
+  late List<dynamic> groupName;
+  late List<dynamic> nameId;
+
+  displayCode(this.groupName, this.nameId);
+
+  @override
+  Widget build(BuildContext context){
+    return SafeArea(child: PlatformScaffold(
+      appBar: PlatformAppBar(title: PlatformText("Code Options")),
+      body: buildList(groupName, nameId),
+    ));
+  }
+}
+
+ListView buildList(List<dynamic> groupName, List<dynamic> nameId){
+  PlatformText("You can copy Group Codes below to share with other members!");
+  return ListView.builder(
+    itemCount: nameId.length,
+    scrollDirection: Axis.vertical,
+    addAutomaticKeepAlives: true,
+    itemBuilder: (_, index){
+      return ListTile (
+        autofocus: true,
+        //leading: Icon(groupIcon.elementAt(index)),  -> to be added after
+        title: PlatformText(groupName.elementAt(index)),
+        subtitle: PlatformText("Copy code here -> ${nameId.elementAt(index)}"),
+      );
+    },
+  );
+}
+
+
