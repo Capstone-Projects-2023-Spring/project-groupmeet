dfec778fd2a18cf2e3679a06ce738101431f49d4
jaredStef <jaredStef@iCloud.com>
2023-04-26 03:00:06 -0400

    Dismiss keyboard when tapping outside text box on onboarding

--- project/lib/onboarding/social_onboarding.dart
+++ project/lib/onboarding/social_onboarding.dart
@@ -97,165 +97,165 @@ class SocialOnboarding extends StatelessWidget {
     }
 
     return PlatformScaffold(
-        body: Center(
+        body: GestureDetector(child: Center(
             child: Column(
-      children: [
-        SizedBox(
-            width: screenWidth,
-            height: MediaQuery.of(context).viewPadding.top +
-                scaleConst * screenHeight),
-        Image.asset(
-          "images/Round3Circles.png",
-          height: 160,
-          width: screenWidth,
-          isAntiAlias: true,
-        ),
-        SizedBox(width: screenWidth, height: 8),
-        PlatformText("Get Around",
-            style: const TextStyle(fontSize: 36, fontWeight: FontWeight.w600)),
-        SizedBox(width: screenWidth, height: 32),
-        Padding(
-            padding:
-                EdgeInsets.symmetric(horizontal: screenWidth / 8, vertical: 0),
-            child: PlatformText(
-              "Save third-party usernames to be used with Round (optional)",
-              textAlign: TextAlign.center,
-              style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
-            )),
-        const SizedBox(height: 32),
-        SizedBox(
-            width: screenWidth * (3 / 4),
-            child: Row(
               children: [
-                Expanded(
-                  flex: 1,
-                  child: Image.asset("images/sms.png",
-                      height: 32, width: 32, isAntiAlias: true),
+                SizedBox(
+                    width: screenWidth,
+                    height: MediaQuery.of(context).viewPadding.top +
+                        scaleConst * screenHeight),
+                Image.asset(
+                  "images/Round3Circles.png",
+                  height: 160,
+                  width: screenWidth,
+                  isAntiAlias: true,
                 ),
-                const SizedBox(width: 8),
-                Expanded(
-                    flex: 8,
-                    child: PlatformTextField(
-                      hintText: "Phone Number",
-                      keyboardType: TextInputType.phone,
-                      onChanged: (p0) => smsChanged(p0),
-                    ))
-              ],
-            )),
-        const SizedBox(height: 16),
-        SizedBox(
-            width: screenWidth * (3 / 4),
-            child: Row(
-              children: [
-                Expanded(
-                    flex: 1,
-                    child: Image.asset(
-                      "images/instagram.png",
-                      height: 32,
-                      width: 32,
-                      isAntiAlias: true,
+                SizedBox(width: screenWidth, height: 8),
+                PlatformText("Get Around",
+                    style: const TextStyle(fontSize: 36, fontWeight: FontWeight.w600)),
+                SizedBox(width: screenWidth, height: 32),
+                Padding(
+                    padding:
+                    EdgeInsets.symmetric(horizontal: screenWidth / 8, vertical: 0),
+                    child: PlatformText(
+                      "Save third-party usernames to be used with Round (optional)",
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 16, fontWeight: FontWeight.w400),
                     )),
-                const SizedBox(width: 8),
+                const SizedBox(height: 32),
+                SizedBox(
+                    width: screenWidth * (3 / 4),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset("images/sms.png",
+                              height: 32, width: 32, isAntiAlias: true),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                            flex: 8,
+                            child: PlatformTextField(
+                              hintText: "Phone Number",
+                              keyboardType: TextInputType.phone,
+                              onChanged: (p0) => smsChanged(p0),
+                            ))
+                      ],
+                    )),
+                const SizedBox(height: 16),
+                SizedBox(
+                    width: screenWidth * (3 / 4),
+                    child: Row(
+                      children: [
+                        Expanded(
+                            flex: 1,
+                            child: Image.asset(
+                              "images/instagram.png",
+                              height: 32,
+                              width: 32,
+                              isAntiAlias: true,
+                            )),
+                        const SizedBox(width: 8),
+                        Expanded(
+                            flex: 8,
+                            child: PlatformTextField(
+                                hintText: "Instagram",
+                                onChanged: (p0) => instagramChanged(p0)))
+                      ],
+                    )),
+                const SizedBox(height: 16),
+                SizedBox(
+                    width: screenWidth * (3 / 4),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset(
+                            "images/snapchat.png",
+                            height: 32,
+                            width: 32,
+                            isAntiAlias: true,
+                          ),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                            flex: 8,
+                            child: PlatformTextField(
+                                hintText: "Snapchat",
+                                onChanged: (p0) => snapChanged(p0)))
+                      ],
+                    )),
+                const SizedBox(height: 16),
+                SizedBox(
+                    width: screenWidth * (3 / 4),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset("images/discord.png",
+                              height: 32, width: 32, isAntiAlias: true),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                            flex: 8,
+                            child: PlatformTextField(
+                                hintText: "Discord",
+                                onChanged: (p0) => discordChanged(p0)))
+                      ],
+                    )),
+                const SizedBox(height: 16),
+                SizedBox(
+                    width: screenWidth * (3 / 4),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Image.asset("images/facebook.png",
+                              height: 32, width: 32, isAntiAlias: true),
+                        ),
+                        const SizedBox(width: 8),
+                        Expanded(
+                            flex: 8,
+                            child: PlatformTextField(
+                                hintText: "Facebook",
+                                onChanged: (p0) => facebookChanged(p0)))
+                      ],
+                    )),
+                const SizedBox(height: 16),
                 Expanded(
-                    flex: 8,
-                    child: PlatformTextField(
-                        hintText: "Instagram",
-                        onChanged: (p0) => instagramChanged(p0)))
-              ],
-            )),
-        const SizedBox(height: 16),
-        SizedBox(
-            width: screenWidth * (3 / 4),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Image.asset(
-                    "images/snapchat.png",
-                    height: 32,
-                    width: 32,
-                    isAntiAlias: true,
+                  child: Align(
+                    alignment: FractionalOffset.bottomCenter,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisSize: MainAxisSize.max,
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        SizedBox(
+                            height: 64,
+                            width: 64,
+                            child: PlatformIconButton(
+                              icon: Image.asset(
+                                "images/OnboardingClap.png",
+                                height: 64,
+                                width: 64,
+                                isAntiAlias: true,
+                              ),
+                              padding: EdgeInsets.zero,
+                              onPressed: () => buttonPress(context),
+                            )),
+                        SizedBox(width: screenWidth, height: 16),
+                        PlatformText("© 2023 Round Corp\nFrom Philly with Love 🤍",
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(fontSize: 10)),
+                        SizedBox(
+                          width: screenWidth,
+                          height: 32,
+                        )
+                      ],
+                    ),
                   ),
                 ),
-                const SizedBox(width: 8),
-                Expanded(
-                    flex: 8,
-                    child: PlatformTextField(
-                        hintText: "Snapchat",
-                        onChanged: (p0) => snapChanged(p0)))
               ],
-            )),
-        const SizedBox(height: 16),
-        SizedBox(
-            width: screenWidth * (3 / 4),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Image.asset("images/discord.png",
-                      height: 32, width: 32, isAntiAlias: true),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                    flex: 8,
-                    child: PlatformTextField(
-                        hintText: "Discord",
-                        onChanged: (p0) => discordChanged(p0)))
-              ],
-            )),
-        const SizedBox(height: 16),
-        SizedBox(
-            width: screenWidth * (3 / 4),
-            child: Row(
-              children: [
-                Expanded(
-                  flex: 1,
-                  child: Image.asset("images/facebook.png",
-                      height: 32, width: 32, isAntiAlias: true),
-                ),
-                const SizedBox(width: 8),
-                Expanded(
-                    flex: 8,
-                    child: PlatformTextField(
-                        hintText: "Facebook",
-                        onChanged: (p0) => facebookChanged(p0)))
-              ],
-            )),
-        const SizedBox(height: 16),
-        Expanded(
-          child: Align(
-            alignment: FractionalOffset.bottomCenter,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                SizedBox(
-                    height: 64,
-                    width: 64,
-                    child: PlatformIconButton(
-                      icon: Image.asset(
-                        "images/OnboardingClap.png",
-                        height: 64,
-                        width: 64,
-                        isAntiAlias: true,
-                      ),
-                      padding: EdgeInsets.zero,
-                      onPressed: () => buttonPress(context),
-                    )),
-                SizedBox(width: screenWidth, height: 16),
-                PlatformText("© 2023 Round Corp\nFrom Philly with Love 🤍",
-                    textAlign: TextAlign.center,
-                    style: const TextStyle(fontSize: 10)),
-                SizedBox(
-                  width: screenWidth,
-                  height: 32,
-                )
-              ],
-            ),
-          ),
-        ),
-      ],
-    )));
+            )), onTap: () => FocusScope.of(context).unfocus(),));
   }
 }
