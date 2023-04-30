a06beab5b9e7893cddd85bcd1c89fa64c933846b
tweiss9 <tylerhweiss@gmail.com>
2023-04-30 14:24:22 -0400

    start of integration test

---
+++ project/test/integration_tests/app_test.dart
@@ -0,0 +1,10 @@
+import 'package:flutter_driver/driver_extension.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('Example', () {
+    test('test', () {
+      expect(1, 1);
+    });
+  });
+}
