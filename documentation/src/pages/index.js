ad53a183545d247da30136fa9163f5ba22c85b3c
KAAzadi <70913312+KAAzadi@users.noreply.github.com>
2023-05-02 16:12:18 -0400

    Update index.js

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -14,14 +14,6 @@ function HomepageHeader() {
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-            {/* TODO: Change me to your project's tutorial*/ }
-          <Link
-            className="button button--secondary button--lg"
-            to="/tutorial/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div>
       </div>
     </header>
   );
